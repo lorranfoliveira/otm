78ec028015e0dceaaa6774c26f4213bd193629d1
Lorran <lorranfoliveira@gmail.com>
2020-08-19 17:37:50 -0300

    Add função para o salvamento de arquivos .npy e .npz

--- otm/manipulacao_arquivos.py
+++ otm/manipulacao_arquivos.py
@@ -1,12 +1,13 @@
 import numpy as np
 import zipfile
 from otm.constantes import ARQUIVOS_DADOS_ZIP
-from typing import Union
+from typing import Union, List
+from loguru import logger
 import os
 from shapely.wkb import loads
 import pathlib
 
-__all__ = ['ler_arquivo_entrada_dados_numpy', 'ler_arquivo_wkb_shapely']
+__all__ = ['ler_arquivo_entrada_dados_numpy', 'salvar_arquivo_numpy', 'ler_arquivo_wkb_shapely']
 
 
 def ler_arquivo_entrada_dados_numpy(arquivo: Union[str, pathlib.Path], n: int) -> Union[np.ndarray, list]:
@@ -26,6 +27,8 @@ def ler_arquivo_entrada_dados_numpy(arquivo: Union[str, pathlib.Path], n: int) -
     """
     # Arquivo do numpy a ser lido
     arq = ARQUIVOS_DADOS_ZIP[n]
+
+    logger.info(f'Lendo "{arq}"...')
 
     # Carregamento do arquivo da estrutura (.zip)
     try:
@@ -56,6 +59,43 @@ def ler_arquivo_entrada_dados_numpy(arquivo: Union[str, pathlib.Path], n: int) -
     except ValueError:
         os.remove(arquivo.parent.joinpath(arq))
         raise ValueError(f'O arquivo "{arq}" não é um arquivo válido do numpy!')
+
+
+def salvar_arquivo_numpy(arquivo: pathlib.Path, dados: Union[np.ndarray, List[np.ndarray]], n: int):
+    """Salva um conjunto de dados em um arquivo do numpy (`.npy` ou `.npz`).
+    O tipo de arquivo a ser salvo é definido automaticamente. Se o conjunto de dados for do tipo
+    `np.ndarray`, será salvo um arquivo `.npy`. Se o conjunto de dados for do tipo `List[np.ndarray]`,
+    será salvo um arquivo com extensão `.npz`.
+
+    Args:
+        arquivo: Nome do arquivo `.zip`.
+        dados: Conjunto de dados a serem salvos.
+        n: Número de referência do arquivo no módulo `constantes`.
+    """
+    arq_np = arquivo.parent.joinpath(ARQUIVOS_DADOS_ZIP[n])
+    arq_np_str = str(arq_np)
+    try:
+        with zipfile.ZipFile(arquivo, 'a', compression=zipfile.ZIP_DEFLATED) as arq_zip:
+            if arquivo.name not in arq_zip.namelist():
+                logger.info(f'Salvando "{arq_np.name}" em "{arquivo.name}..."')
+
+                if arq_np_str.endswith('.npy'):
+                    if isinstance(dados, np.ndarray):
+                        np.save(str(arq_np_str), dados)
+                    else:
+                        raise TypeError(f'Para salvar um arquivo ".npy" é necessário que "dados" seja '
+                                        f'do tipo "np.ndarray".')
+                elif arq_np_str.endswith('.npz'):
+                    if isinstance(dados, list) and all(isinstance(i, np.ndarray) for i in dados):
+                        np.savez(arq_np_str, *dados)
+                    else:
+                        raise TypeError(f'Para salvar um arquivo ".npz" é necessário que "dados" seja '
+                                        f'do tipo "List[np.ndarray]".')
+
+                arq_zip.write(arq_np.name)
+                os.remove(arq_np_str)
+    except FileNotFoundError:
+        raise FileNotFoundError(f'O arquivo "{arquivo}" com os dados da estrutura não existe!')
 
 
 def ler_arquivo_wkb_shapely(arquivo: Union[str, pathlib.Path], n: int = 10):
